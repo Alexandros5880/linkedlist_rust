990ff212d3b01f67598ffe4c668c3f2553be2487
Αλέξανδρος Πλατάνιος <Alexpl_15@windowslive.com>
2023-03-23 19:24:29 +0200

    Push_Front, Push_Back, Delete_By_Index, Get_By_Index && Test OK.

--- src/lib.rs
+++ src/lib.rs
@@ -1,84 +1,284 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+
 use std::rc::Weak;
 use std::{fmt::Display, rc::Rc, cell::RefCell};
 use std::fmt::Debug;
 
 #[derive(Debug)]
-pub struct Node<T>
-  where T: Display + Debug 
+struct Node<T>
+  where T: Display + Debug + Copy
 {
-  pub previus: Option<Weak<RefCell<Node<T>>>>,
+  pub prev: Option<Weak<RefCell<Node<T>>>>,
   pub next: Option<Rc<RefCell<Node<T>>>>,
   pub data: T
 }
 
+impl<T> Node<T>
+  where T: Display + Debug + Copy
+{
+  pub fn new(data: T) -> Rc<RefCell<Self>> {
+    Rc::new(RefCell::new(Node {
+      prev: None,
+      next: None,
+      data
+    }))
+  }
+}
+
+
 #[derive(Debug)]
-pub struct LinkedList<T: Display + Debug> {
-  pub root: Rc<RefCell<Node<T>>>
+pub struct LinkedList<T>
+  where T: Display + Debug + Copy
+{
+  head: Option<Rc<RefCell<Node<T>>>>,
+  tail: Option<Rc<RefCell<Node<T>>>>,
+  pub len: usize,
+  index: usize
 }
 
-impl<T: Display + Debug> LinkedList<T> {
+impl<T> LinkedList<T>
+  where T: Display + Debug + Copy
+{
 
-  pub fn new(data: T) -> LinkedList<T> {
-    let node = Node {
-      previus: None,
-      next: None,
-      data
-    };
+  pub fn new() -> LinkedList<T> {
     LinkedList {
-      root: Rc::new(RefCell::new(node))
+      head: None,
+      tail: None,
+      len: 0,
+      index: 0
     }
   }
 
-  pub fn Push(&mut self, data: T) {
+  pub fn push_front(&mut self, elem: T) {
+    self.len += 1;
+    let new_head = Node::new(elem);
+    match self.head.take() {
+        Some(old_head) => {
+            old_head.borrow_mut().prev = Some(Rc::downgrade(&new_head.clone()));
+            new_head.borrow_mut().next = Some(old_head.to_owned());
+            self.head = Some(new_head);
+        }
+        None => {
+            self.tail = Some(new_head.clone());
+            self.head = Some(new_head);
+        }
+    }
+  }
 
-    let next = Rc::from(RefCell::new(Node {
-      previus: None,
-      next: None,
-      data
-    }));
+  pub fn push_back(&mut self, elem: T) {
+    self.len += 1;
+    let new_tail = Node::new(elem);
+    match self.tail.take() {
+      Some(old_tail) => {
+        old_tail.borrow_mut().next = Some(new_tail.to_owned());
+        new_tail.borrow_mut().prev = Some(Rc::downgrade(&old_tail.clone()));
+        self.tail = Some(new_tail);
+      }
+      None => {
+        self.head = Some(new_tail.clone());
+        self.tail = Some(new_tail);
+      }
+    }
+  }
 
-    let tail = self.Tail(Rc::clone(&self.root));
+  pub fn delete_by_index(&mut self, index: usize) {
+    let node: Option<Rc<RefCell<Node<T>>>>;
+    if index == 0 {
+      node = self.head.clone();
+    } else if index == self.len - 1 {
+      node = self.tail.clone();
+    } else if index <= self.len / 2 {
+      self.index = 0;
+      let index = index + 1;
+      node = self.get_by_index_from_head(self.head.clone(), index);
+    } else {
+      self.index = self.len + 1;
+      let index = index + 1;
+      node = self.get_by_index_from_tail(self.tail.clone(), index);
+    }
     
-    next.borrow_mut().previus = Some(Rc::downgrade(&tail));
-    tail.borrow_mut().next = Some(next);
+    let unwraped = node.unwrap();
+
+    let prev = unwraped.borrow().prev.clone();
+    let prev = if prev.is_none() {
+      unwraped.clone()
+    } else {
+      prev.unwrap().upgrade().unwrap()
+    };
+    let mut prev_mut = prev.borrow_mut();
+
+    let next = unwraped.borrow().next.clone();
+    let next = if next.is_none() {
+      unwraped.clone()
+    } else {
+      next.unwrap()
+    };
+    let mut next_mut = next.borrow_mut();
+    
+    prev_mut.next = Some(next.to_owned());
+    next_mut.prev = Some(Rc::downgrade(&prev.to_owned()));
+    
+    self.len -= 1;
   }
 
-  fn Tail(&self, node: Rc<RefCell<Node<T>>>) -> Rc<RefCell<Node<T>>> {
-    match &node.borrow().next {
-        Some(node) => self.Tail(Rc::clone(&node)),
-        None => Rc::clone(&node)
+  pub fn get_by_index(&mut self, index: usize) -> Option<T> {
+    let node: Option<Rc<RefCell<Node<T>>>>;
+    if index == 0 {
+      node = self.head.clone();
+    } else if index == self.len-1 {
+      node = self.tail.clone();
+    } else if index <= self.len / 2 {
+      self.index = 0;
+      let index = index + 1;
+      node = self.get_by_index_from_head(self.head.clone(), index);
+    } else {
+      self.index = self.len + 1;
+      let index = index + 1;
+      node = self.get_by_index_from_tail(self.tail.clone(), index);
+    }
+
+    let result = match node {
+      Some(data) => Some(data.borrow().data.clone()),
+      None => None
+    };
+
+    result
+  }
+
+  fn get_by_index_from_head(&mut self, node: Option<Rc<RefCell<Node<T>>>>, index: usize) -> Option<Rc<RefCell<Node<T>>>> {
+    let n = node.clone();
+    self.index += 1;
+    if node.is_some() {
+      if index == self.index {
+        n
+      } else {
+        let next = n.unwrap().borrow().next.clone();
+        self.get_by_index_from_head(next, index)
+      }
+    } else {
+      None
     }
   }
 
-  fn Head(&self, node: Rc<RefCell<Node<T>>>) -> Rc<RefCell<Node<T>>> {
-    match &node.borrow().previus {
-        Some(node_weak) => {
-          match node_weak.upgrade() {
-            Some(node_rc) => self.Head(Rc::clone(&node_rc)),
-            None => Rc::clone(&node)
-          }
-        },
-        None => Rc::clone(&node)
+  fn get_by_index_from_tail(&mut self, node: Option<Rc<RefCell<Node<T>>>>, index: usize) -> Option<Rc<RefCell<Node<T>>>> {
+    let n = node.clone();
+    self.index -= 1;
+    if node.is_some() {
+      if index == self.index {
+        n
+      } else {
+        let prev = n.unwrap().borrow().prev.clone();
+        if prev.is_some() {
+          let prev = prev.unwrap().upgrade();
+          self.get_by_index_from_tail(prev, index)
+        } else {
+          None
+        }
+      }
+    } else {
+      None
     }
   }
 
 }
 
-// impl<T: Display + Debug> Iterator for LinkedList<T> {
-//     type Item;
 
-//     fn next(&mut self) -> Option<Self::Item> {
-//         todo!()
+// impl<T> Iterator for LinkedList<T>
+//   where T: Display + Debug + Copy + Default
+// {
+//   type Item = T;
+
+//   fn next(&mut self) -> Option<Self::Item> {
+
+//     if self.root.borrow().next.is_some() {
+//       let root = self.root.as_ref();
+//       let next = root.clone().into_inner();
+//       *self.root.borrow_mut() = next;
+//       Some(self.root.borrow().data.to_owned())
+//     } else {
+//       None
 //     }
+
+//   }
 // }
+
 
 
 #[cfg(test)]
 mod tests {
-    //use super::*;
+  use crate::LinkedList;
 
-    #[test]
-    fn it_works() {
-        assert_eq!(4, 4);
-    }
+  #[test]
+  fn push_back_works() {
+    let mut list = LinkedList::new();
+    list.push_back(0);
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    list.push_back(4);
+    list.push_back(5);
+    list.push_back(6);
+    list.push_back(7);
+
+    assert_eq!(list.len, 8);
+  }
+
+  #[test]
+  fn push_front_works() {
+    let mut list = LinkedList::new();
+    list.push_front(0);
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
+    list.push_front(4);
+    list.push_front(5);
+    list.push_front(6);
+    list.push_front(7);
+
+    assert_eq!(list.len, 8);
+  }
+
+  #[test]
+  fn delete_works() {
+    let mut list = LinkedList::new();
+    list.push_back(0);
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    list.push_back(4);
+    list.push_back(5);
+    list.push_back(6);
+    list.push_back(7);
+
+    list.delete_by_index(3);
+    assert_eq!(list.len, 7);
+
+    let data = list.get_by_index(3);
+    assert_eq!(data.is_some(), true);
+    let unwraped = data.unwrap();
+    assert_eq!(unwraped, 4);
+  }
+
+  #[test]
+  fn get_by_index_works() {
+    let mut list = LinkedList::new();
+    list.push_back(0);
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+    list.push_back(4);
+    list.push_back(5);
+    list.push_back(6);
+    list.push_back(7);
+
+    let data = list.get_by_index(4);
+    assert_eq!(data.is_some(), true);
+    let unwraped = data.unwrap();
+    assert_eq!(unwraped, 4);
+
+    let data = list.get_by_index(9);
+    assert_eq!(data.is_none(), true);
+  }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
 use linkedlist::LinkedList;
 
 pub fn main() {
-  let mut list = LinkedList::new(1);
-  list.Push(2);
-  list.Push(3);
-  list.Push(4);
-  list.Push(5);
-  list.Push(6);
-  list.Push(7);
+  let mut list = LinkedList::new();
+  list.push_back(0);
+  list.push_back(1);
+  list.push_back(2);
+  list.push_back(3);
+  list.push_back(4);
+  list.push_back(5);
+  list.push_back(6);
+  list.push_back(7);
 
-  println!("List: {:#?}", &list);
+  println!("{:#?}", list);
+
+  let index = 7;
+  let data = list.get_by_index(index);
+  match data {
+    Some(data) => println!("list[{}]: {:?}", &index, data),
+    None => println!("Out of Index!")
+  }
+  
+
+
+  // let data = list.get_by_index(4);
+  // assert_eq!(data.is_some(), true);
+  // let unwraped = data.unwrap();
+  // println!("\n\n{}\n\n", unwraped);
+
+  // for item in list {
+  //   println!("Item: {:#?}", item);
+  // }
 }
